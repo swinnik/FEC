01c007bc9f0146411c5d9fcf828e42d768b40663
Sean Winnik <winnik.sean@gmail.com>
2023-05-29 10:56:58 -0700

    UPDATE REPORTED, through putReport successful

--- server/controllers/reviews.js
+++ server/controllers/reviews.js
@@ -4,29 +4,28 @@ const models = require('../models');
 const { ATELIER_API, API_TOKEN } = process.env;
 
 module.exports = {
-  // get: (req, res) => {
-  //   axios.get(`${ATELIER_API}/reviews`, {
-  //     params: {
-  //       page: req.query.page,
-  //       count: req.query.count,
-  //       sort: req.query.sort,
-  //       product_id: req.query.product_id,
-  //     },
-  //     headers: {
-  //       authorization: API_TOKEN,
-  //     },
-  //   })
-  //     .then(({ data }) => res.json(data.results))
-  //     .catch((err) => {
-  //       console.log('ERROR GETTING REVIEWS', err);
-  //       res.status(404).json(err);
-  //     });
-  // },
+    // get: (req, res) => {
+    //   axios.get(`${ATELIER_API}/reviews`, {
+    //     params: {
+    //       page: req.query.page,
+    //       count: req.query.count,
+    //       sort: req.query.sort,
+    //       product_id: req.query.product_id,s
+    //     },
+    //     headers: {
+    //       authorization: API_TOKEN,
+    //     },
+    //   })
+    //     .then(({ data }) => res.json(data.results))
+    //     .catch((err) => {
+    //       console.log('ERROR GETTING REVIEWS', err);
+    //       res.status(404).json(err);
+    //     });
+    // },
 
   get: (req, res) => {
     models.review.getAllReviews(req, res)
     .then((dbRes)=> {
-        console.log("SDFLSDF", dbRes, 'asdfasdfl;askdjf;alskdf')
         res.send(dbRes.rows);
       })
       .catch((err) => {
@@ -68,19 +67,8 @@ module.exports = {
   getMeta: (req, res) => {
     models.review.getMeta(req, res)
       .then((dbRes)=> {
-        console.log('start metadata res controller:', dbRes, 'METADATA res controller end')
-        // const dada = {
-        //   product_id: '40346',
-        //   ratings: { '1': '24', '2': '54', '3': '53', '4': '41', '5': '91' },
-        //   recommended: { false: '70', true: '193' },
-        //   characteristics: {
-        //     Fit: { id: 135224, value: '2.8415841584158416' },
-        //     Length: { id: 135225, value: '3.1176470588235294' },
-        //     Comfort: { id: 135226, value: '3.0000000000000000' },
-        //     Quality: { id: 135227, value: '3.3800000000000000' }
-        //   }}
+        // console.log('start metadata res controller:', dbRes, 'METADATA res controller end')
         // console.log('CONTROLLER DATATA GET META start:', dbRes.rows, 'DATATA CONTROLLER GET META end');
-        // res.send(dbRes.rows);
         res.send(dbRes)
       })
       .catch((err) => {
@@ -112,16 +100,26 @@ module.exports = {
         res.sendStatus(400);
       });
   },
+  // putReport: (req, res) => {
+  //   axios.put(`${ATELIER_API}/reviews/${req.params.review_id}/report`, {}, {
+  //     headers: {
+  //       authorization: API_TOKEN,
+  //     },
+  //   })
+  //     .then(() => res.sendStatus(204))
+  //     .catch((err) => {
+  //       console.error('ERROR REPORTING REVIEW', err);
+  //       res.sendStatus(400);
+  //     });
+  // },
   putReport: (req, res) => {
-    axios.put(`${ATELIER_API}/reviews/${req.params.review_id}/report`, {}, {
-      headers: {
-        authorization: API_TOKEN,
-      },
-    })
-      .then(() => res.sendStatus(204))
+    console.log('PUTREPORT REQ START', req, 'PUT REPORT REQ END');
+    models.review.putReport(req, res)
+      .then((dbRes)=> {
+        console.log('PUTREPORT START controller dbRES reported  ', dbRes, 'PUT REPORT dbRes controller end')
+      })
       .catch((err) => {
-        console.error('ERROR REPORTING REVIEW', err);
-        res.sendStatus(400);
+        console.log('error in CONTROLLER GET META', err);
       });
   },
 };
--- server/models/seanModels.js
+++ server/models/seanModels.js
@@ -20,7 +20,7 @@ module.exports = {
           sort = 'date DESC';
       }
 
-      product_id = product_id || 2;
+      product_id = product_id;
       page = page || 1;
       count = count || 5;
       if (!product_id || !sort) {
@@ -30,12 +30,15 @@ module.exports = {
       await pool.connect();
       data = await pool.query(
         `SELECT review.id AS review_id, array_agg(json_build_object('id', review_photos,  'url', review_photos.url))
-        as photos, rating,  summary, recommended, response,  body,
-        TO_TIMESTAMP(date/1000) as date, reviewer_name, helpfulness FROM review FULL OUTER JOIN
-        review_photos ON review_photos.review_id = review.id WHERE review.product_id= $1 AND review.
+        as photos, rating,  summary, recommended, reported, response,  body,
+        TO_TIMESTAMP(date/1000) as date, reviewer_name, helpfulness
+        FROM review FULL OUTER JOIN review_photos
+        ON review_photos.review_id = review.id
+        WHERE review.product_id= $1 AND review.
         reported = false GROUP BY review.id ORDER BY ${sort} OFFSET ${(page - 1) * count} LIMIT ${count};`,
         [product_id]
       );
+      console.log('MODELGET REVIEWS start ', data.rows, ' MODEL GET REVIEWS end')
       return data
     } catch (err) {
       console.log(err);
@@ -45,14 +48,14 @@ module.exports = {
   getMeta: async (req, res) => {
     try {
       let { product_id } = req.query;
-      console.log(req.query, 'META QUERY PARAMS');
-      product_id = product_id;
+      product_id = product_id || 2;
       if (!product_id) {
         throw new Error(`Missing headers. Received product_id: ${product_id}`);
       }
+      console.log(req.query, 'META QUERY PARAMS');
       let data_rating = await pool.query(
         `SELECT rating, COUNT(*) AS count FROM review
-         WHERE product_id = ${product_id} GROUP BY rating;`,
+        WHERE product_id = ${product_id} GROUP BY rating;`,
       );
       let dataRating = {}
       data_rating.rows.forEach(e=>{
@@ -66,13 +69,15 @@ module.exports = {
       data_recommend.rows.forEach(e=>{
         dataRecommend[e.recommended] = e.count;
       })
-      const data_characteritics =  await pool.query(
-        `SELECT c.name, c.id, AVG(cr.value) AS average_value FROM characteristics c
-        JOIN characteristic_reviews cr ON c.id = cr.characteristic_id
+      const data_characteristics =  await pool.query(
+        `SELECT c.name, c.id, AVG(cr.value) AS average_value
+        FROM characteristics c
+        JOIN characteristic_reviews cr
+        ON c.id = cr.characteristic_id
         WHERE c.product_id = ${product_id} GROUP BY c.name, c.id;`,
       );
       let dataCharacteristics = {};
-      data_characteritics.rows.forEach(e=>{
+      data_characteristics.rows.forEach(e=>{
         dataCharacteristics[e.name] = {id: e.id, value: e.average_value}
       })
       const collatedData = {
@@ -88,95 +93,30 @@ module.exports = {
       res.status(500).send('***ERROR RETRIEVING REVIEWS***');
     }
   },
+  putReport: async (req, res) => {
+    try {
+      const {review_id} = req.params;
+      console.log(req, "PUT REPORT PUT REPORT PUT REPORT ")
+      if (!review_id) {
+        throw new Error(`Missing headers. Received review_id: ${review_id}`);
+      }
+      console.log(review_id, 'META QUERY PARAMS');
+      let update_report = await pool.query(
+        `UPDATE review
+        SET reported = true
+        WHERE id = ${review_id}`,
+      );
+      let reported = await pool.query(
+        `SELECT reported FROM review WHERE id  = ${review_id}`
+      )
+      res.status(201).send('Review Has Been Reported by User')
+      return reported.rows
+    } catch (err) {
+      console.log(err);
+      res.status(500).send('***ERROR RETRIEVING REVIEWS***');
+    }
+  },
+
+
 };
 
-
-
-/* eslint-disable camelcase */
-/* eslint-disable no-console */
-// const pool = require('../db/db');
-
-// module.exports = {
-
-//   getAllReviews: (req,res) => {
-//     try {
-//       let { page, count, sort, product_id} = req.query;
-//       console.log(req.query, 'QUERY PARAMS')
-//       switch (sort) {
-//         case 'newest':
-//           sort = 'date DESC';
-//           break;
-//         case 'helpful':
-//           sort = 'helpfulness DESC';
-//           break;
-//         case 'relevant':
-//           sort = 'helpfulness DESC';
-//           break;
-//         default:
-//           sort = 'date DESC';
-//       }
-
-//       product_id = product_id || 2;
-//       page = page || 1;
-//       count = count || 5;
-//       if (!product_id || !sort) {
-//         throw new Error(`Missing headers. Received product_id: ${product_id}, sort: ${sort}`)
-//       }
-
-//       pool.connect()
-//         .then(() => {
-//           pool.query(`SELECT review.id AS review_id, array_agg(json_build_object('id', review_photos,  'url', review_photos.url)) as photos, rating,  summary, recommended, response,  body,
-//             TO_TIMESTAMP(date/1000) as date, reviewer_name, helpfulness FROM review FULL OUTER JOIN
-//             review_photos ON review_photos.review_id = review.id WHERE review.product_id= $1 AND review.
-//             reported = false GROUP BY review.id ORDER BY ${sort} OFFSET ${(page - 1) * count} LIMIT ${count};`, [product_id])
-//             .then(data=> {
-//               console.log(data, 'DATATATA ');
-//               res.status(200).send({
-//                 product: product_id,
-//                 page: page,
-//                 count: count,
-//                 results: data.rows,
-//               });
-//             });
-//         })
-//         .catch((err) => {
-//           console.log(err)
-//           res.status(500).send('ERROR RETREIVING REVIEWS')
-//         });
-//     } catch (err) {
-//       console.log(err)
-//       res.status(500).send('***COUDL NOT RETRIEVE REVIEWS***');
-//     }
-//   },
-
-// }
-
-
-//   (req, res) => {
-//     const {page, count, sort, product_id} = req
-//     console.log(req, 'MODEL MODEL MODEL')
-//     const offset = (page - 1) * count;
-//     let sorter;
-//     if (sort === 'newest') { sorter = 'date'; }
-//     if (sort === 'helpful') { sorter = 'recommend'; }
-//     if (sort === 'relevant') { sorter = 'recommend'; }
-//     else {sorter = 'date'}
-
-//     const query = {
-//       text: `
-//       SELECT *
-//       FROM review
-//       WHERE product_id = $1
-//       ORDER BY ${sorter} DESC
-//       LIMIT $2 OFFSET $3;
-//       `,
-//       values: [product_id, count, offset],
-//     };
-//     return pool.query(query)
-//       .then((res) => res.rows)
-//       .catch((err) => {
-//         console.log('error querying database', err);
-//         throw err;
-//       });
-//   },
-// };
